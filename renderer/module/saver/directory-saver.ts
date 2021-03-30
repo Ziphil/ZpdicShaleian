3abe784a8df5d6af9b647d5acca20c232f19c05e
Ziphil <yfe23457@nifty.com>
2021-03-30 22:48:06 +0900

    辞書の保存時に古いデータを削除するよう変更

--- renderer/module/saver/directory-saver.ts
+++ renderer/module/saver/directory-saver.ts
@@ -23,6 +23,8 @@ export class DirectorySaver extends Saver {
 
   private size: number = 0;
   private count: number = 0;
+  private deleteSize: number = 0;
+  private deleteCount: number = 0;
   private settingsSaved: boolean = false;
   private markersSaved: boolean = false;
 
@@ -31,9 +33,44 @@ export class DirectorySaver extends Saver {
   }
 
   public start(): void {
+    this.deleteOlds();
+  }
+
+  private deleteOlds(): void {
+    this.size = this.dictionary.words.length;
+    fs.readdir(this.path, (error, paths) => {
+      if (error) {
+        this.emit("error", error);
+      } else {
+        try {
+          let oldLocalPaths = paths.filter((path) => path.endsWith(".nxdw") || path.endsWith(".nxds"));
+          this.deleteSize = oldLocalPaths.length;
+          for (let oldLocalPath of oldLocalPaths) {
+            let oldPath = path.join(this.path, oldLocalPath);
+            this.deleteOld(oldPath);
+          }
+        } catch (error) {
+          this.emit("error", error);
+        }
+      }
+    });
+  }
+
+  private deleteOld(path: string): void {
+    fs.unlink(path, (error) => {
+      if (error) {
+        this.emit("error", error);
+      } else {
+        this.deleteCount ++;
+        this.emitProgress();
+        this.checkDeleteEnded();
+      }
+    });
+  }
+
+  private saveWords(): void {
     let dictionary = this.dictionary;
     let words = this.dictionary.words;
-    this.size = words.length;
     fs.mkdir(this.path, {recursive: true}, (error) => {
       if (error) {
         this.emit("error", error);
@@ -93,6 +130,12 @@ export class DirectorySaver extends Saver {
     });
   }
 
+  private checkDeleteEnded(): void {
+    if (this.deleteCount >= this.deleteSize) {
+      this.saveWords();
+    }
+  }
+
   private checkEnded(): void {
     if (this.count >= this.size && this.settingsSaved && this.markersSaved) {
       this.emit("end");
@@ -100,7 +143,7 @@ export class DirectorySaver extends Saver {
   }
 
   private emitProgress(): void {
-    this.emit("progress", this.count, this.size);
+    this.emit("progress", this.count + this.deleteCount, this.size + this.deleteSize);
   }
 
 }
