5e45167667a5f4b80521580adc4392f3088f694a
Ziphil <yfe23457@nifty.com>
2021-04-03 13:22:36 +0900

    シンタックスハイライトの規則を修正

--- renderer/util/code-mirror.ts
+++ renderer/util/code-mirror.ts
@@ -7,20 +7,20 @@ import "codemirror/addon/mode/simple";
 CodeMirror.defineSimpleMode("shcontent", {
   start: [
     {regex: /\+|=\??|\-|\w\??:/, sol: true, token: "operator"},
-    {regex: /(<)(.+?)(>)/, token: "tag"},
+    {regex: /(<)(.*?)(>)/, token: "tag"},
     {regex: /@\d+/, token: "date"},
     {regex: /\{/, token: "markup", next: "brace"},
     {regex: /\[/, token: "markup", next: "bracket"},
-    {regex: /(\/)(.+?)(\/)/, token: ["markup", null as any, "markup"]},
+    {regex: /(\/)(.*?)(\/)/, token: ["markup", null as any, "markup"]},
     {regex: /`./, token: "escape"}
   ],
   brace: [
-    {regex: /(\/)(.+?)(\/)/, token: ["markup", null as any, "markup"]},
+    {regex: /(\/)(.*?)(\/)/, token: ["markup", null as any, "markup"]},
     {regex: /`./, token: "escape"},
     {regex: /\}/, token: "markup", next: "start"}
   ],
   bracket: [
-    {regex: /(\/)(.+?)(\/)/, token: ["markup", null as any, "markup"]},
+    {regex: /(\/)(.*?)(\/)/, token: ["markup", null as any, "markup"]},
     {regex: /`./, token: "escape"},
     {regex: /\]/, token: "markup", next: "start"}
   ]
