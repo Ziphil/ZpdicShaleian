30b3e636abb56d514e0e1b52aa084290a744b83e
Ziphil <yfe23457@nifty.com>
2021-04-09 15:22:51 +0900

    不要なコメントアウトを削除

--- renderer/component/compound/word-list.tsx
+++ renderer/component/compound/word-list.tsx
@@ -28,12 +28,6 @@ import {
 @component()
 export class WordList extends Component<Props, State> {
 
-  // public componentDidUpdate(previousProps: any): void {
-  //   if (this.props.words !== previousProps.words || this.props.page !== previousProps.page) {
-  //     document.getElementById("word-list-container")!.scrollTop = 0;
-  //   }
-  // }
-
   private handlePageSet(page: number) {
     if (this.props.onPageSet) {
       this.props.onPageSet(page);
